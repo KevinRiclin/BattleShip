010488183c2bce174b8f252428c37066e541bb56
KevinRiclin <kevin@DESKTOP-KHVL9V7.localdomain>
2026-02-17 23:20:12 +0100

    Starting project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// Point d'entrée principal de l'application
+console.log('Bataille Navale - Application démarrée');
+
+// Initialisation du jeu
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM chargé');
+    // Logique du jeu sera ajoutée ici
+});
